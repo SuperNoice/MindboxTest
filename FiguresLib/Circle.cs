c91075c442714b2224fcf5139a2d6d9c33a0c798
SuperNoice <lozovik.ru@mail.ru>
2023-08-21 21:47:50 +0300

    Add Circle and Triangle classes

--- FiguresLib/Circle.cs
+++ FiguresLib/Circle.cs
@@ -2,9 +2,16 @@
 {
     public class Circle : IFigure
     {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
         public double GetArea()
         {
-            throw new NotImplementedException();
+            return Math.PI * Math.Pow(Radius, 2);
         }
     }
 }
--- FiguresLib/Triangle.cs
+++ FiguresLib/Triangle.cs
@@ -2,9 +2,32 @@
 {
     public class Triangle : IFigure
     {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public bool IsRight => CheckRight();
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
         public double GetArea()
         {
-            throw new NotImplementedException();
+            // Полупериметр
+            var p = (SideA + SideB + SideC) / 2.0d;
+
+            return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+        }
+
+        private bool CheckRight()
+        {
+            var Sides = new double[] { SideA, SideB, SideC };
+            Array.Sort(Sides);
+
+            return (Math.Pow(Sides[0], 2) + Math.Pow(Sides[1], 2)) == Math.Pow(Sides[2], 2);
         }
     }
 }
